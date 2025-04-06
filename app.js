31d08a064ce0ebf4466519e22c1778d41ac2dd0a
KevinGr <kgomezr2@unicartagena.edu.co>
2025-04-06 14:18:26 -0500

    Guardando acceso a la base de datos

--- app.js
+++ app.js
@@ -26,9 +26,9 @@ const port = process.env.PORT || 8080;
 const app = express()
 
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "[https://cf-udc.netlify.app/]").setHeader('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
   });
