d1435ae4846f05db673fecd1ce9ac374b3698815
KevinGr <kgomezr2@unicartagena.edu.co>
2025-04-06 13:16:13 -0500

    importando compañero desde netlify functions

--- netlify/functions/classfriends/classfriends.mjs
+++ netlify/functions/classfriends/classfriends.mjs
@@ -1,11 +1,18 @@
 // Docs on request and context https://docs.netlify.com/functions/build/#code-your-function-2
-export default (request, context) => {
+export default async (request, context) => {
   try {
     
     const url = new URL(request.url)
-    const subject = url.searchParams.get('name') || 'World'
+    const subject = url.searchParams.get('id') || 'World'
 
-    return new Response(`Hello ${subject}`)
+    const response = await fetch(`https://app-3b8e40df-de09-4d4e-a795-9d05e9806eef.cleverapps.io/classfriend/${subject}`)
+    const data = await response.json()
+    return new Response(JSON.stringify(data), {
+      status: 200,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
   } catch (error) {
     return new Response(error.toString(), {
       status: 500,
