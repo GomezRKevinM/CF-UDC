1cd0d33e2463d18aa5ebb5a99818371eb50dd34d
KevinGr <kgomezr2@unicartagena.edu.co>
2025-04-06 13:52:06 -0500

    cambios en la función

--- netlify/functions/new-user/new-user.mjs
+++ netlify/functions/new-user/new-user.mjs
@@ -1,12 +1,16 @@
 // Docs on request and context https://docs.netlify.com/functions/build/#code-your-function-2
 export default async (request, context) => {
   try {
-    const url = new URL(request.url)
-    const id = url.searchParams.get('id') || 0
-
-    fetch(`https://app-3b8e40df-de09-4d4e-a795-9d05e9806eef.cleverapps.io/classfriend/${id}`)
+    fetch(`https://app-3b8e40df-de09-4d4e-a795-9d05e9806eef.cleverapps.io/new-user`)
     .then(response => response.json())
     .then(data => {
+        swal.fire({
+            title: "Usuario creado",
+            text: "El usuario ha sido creado exitosamente",
+            icon: "success",
+            timer: 2000,
+            showConfirmButton: false,
+        })
         return new Response(JSON.stringify(data), {
             status: 200,
             headers: {
@@ -15,6 +19,14 @@ export default async (request, context) => {
         })
     })
   } catch (error) {
+    swal.fire({
+        title: "Error",
+        text: "Error al crear el usuario",
+        icon: "error",
+        timer: 2000,
+        showConfirmButton: false,
+    })
+    console.error('Error:', error)
     return new Response(error.toString(), {
       status: 500,
     })
