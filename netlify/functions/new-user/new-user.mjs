72528858054fc443a8accd56b72e38b2e248c955
KevinGr <kgomezr2@unicartagena.edu.co>
2025-04-07 10:30:12 -0500

    cambios

--- netlify/functions/new-user/new-user.mjs
+++ netlify/functions/new-user/new-user.mjs
@@ -1,23 +1,40 @@
 // Docs on request and context https://docs.netlify.com/functions/build/#code-your-function-2
 export default async (request, context) => {
   try {
+    const body = await request.json()
+    const { nombre, username, password, emailU, emailP, number } = body
+    console.log(body)
+    if (!nombre || !username || !password || !emailU || !emailP || !number) {
+      return new Response('Faltan datos', { status: 400 })
+    }
+
     fetch(`https://app-3b8e40df-de09-4d4e-a795-9d05e9806eef.cleverapps.io/new-user`)
-    .then(response => response.json())
-    .then(data => {
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Error en la solicitud')
+        }
+        return response.json()
+      })
+      .then(data => {
+        console.log(data)
         swal.fire({
-            title: "Usuario creado",
-            text: "El usuario ha sido creado exitosamente",
-            icon: "success",
-            timer: 2000,
-            showConfirmButton: false,
+          title: "Usuario creado",
+          text: "El usuario ha sido creado correctamente",
+          icon: "success",
+          timer: 2000,
+          showConfirmButton: false,
         })
-        return new Response(JSON.stringify(data), {
-            status: 200,
-            headers: {
-                'Content-Type': 'application/json',
-            },
+      })
+      .catch(error => {
+        console.error('Error:', error)
+        swal.fire({
+          title: "Error",
+          text: "Error al crear el usuario",
+          icon: "error",
+          timer: 2000,
+          showConfirmButton: false,
         })
-    })
+      })
   } catch (error) {
     swal.fire({
         title: "Error",
